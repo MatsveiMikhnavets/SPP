21c78305b610d55223c7c093263f325398a4894c
MatsveiMikhnavets <71584730+MatsveiMikhnavets@users.noreply.github.com>
2021-12-08 17:38:34 +0300

    Add files via upload

---
+++ SPP1/SPP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace SPP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskQueue taskQueue = new TaskQueue(10);
+
+            for (int i = 0; i < 20; i++)
+            {
+                taskQueue.EnqueueTask(Test);
+            }
+        }
+
+        static void Test()
+        {
+            int k = 0;
+            for (int i = 0; i < 1000000; i++)
+            {
+                k++;
+            }
+            Console.WriteLine(Thread.CurrentThread.Name);
+        }
+    }
+}
---
+++ SPP1/SPP1/TaskQueue.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace SPP1
+{
+    public class TaskQueue
+    {
+        public delegate void TaskDelegate();
+
+        private readonly Queue<TaskDelegate> _tasksQueue = new();
+
+        public TaskQueue(int threadsCount)
+        {
+            var threads = new Thread[threadsCount];
+
+            for (int i = 0; i < threadsCount; i++)
+            {
+                var thread = new Thread(ProcessThreads) { Name = $"{i + 1}" };
+                thread.Start();
+                threads[i] = thread;
+            }
+        }
+
+        private void ProcessThreads()
+        {
+            while (Thread.CurrentThread.IsAlive)
+            {
+                TaskDelegate task = null;
+                lock (_tasksQueue)
+                {
+                    if (_tasksQueue.Count > 0)
+                    {
+                        task = _tasksQueue.Dequeue();
+                    }
+                }
+                task?.Invoke();
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            lock (_tasksQueue)
+            {
+                _tasksQueue.Enqueue(task);
+            }
+        }
+    }
+}
